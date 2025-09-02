743c382a4761f2d18be263e2e299725924cc4808
Skok Oleg <o.skok@yandex.ru>
2025-09-02 11:44:47 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -1,22 +1,35 @@
-  typeset -r PACKAGES=(
-    'ansible'
-    'ansible-lint'
-    'molecule'
-    'molecule-docker'
-    'tox'
-    'tox-ansible'
-    'hvac'
-    'dnspython'
-    'jmespath'
-    'urllib3<2.0'
-  )
+# =============================================================================
+#   ZSH ANSIBLE PLUGIN
+# =============================================================================
 
-if type python3 >/dev/null; then
+_zsh_package_pip {
+  $(which python3) -m pip install --upgrade pip --user
+  rehash
+}
+
+_zsh_package_ansible {
+    typeset -r PACKAGES=(
+      'ansible'
+      'ansible-lint'
+      'molecule'
+      'molecule-docker'
+      'tox'
+      'tox-ansible'
+      'hvac'
+      'dnspython'
+      'jmespath'
+      'urllib3<2.0'
+    )
+
   for package in "${PACKAGES[@]}"; do
-    $(which python3) -m pip install --upgrade pip
     $(which python3) -m pip install --upgrade ${package} --user
     rehash
   done
+}
+
+if type python3 >/dev/null; then
+  _zsh_package_pip
+  _zsh_package_ansible
 fi
 
 if type molecule >/dev/null; then
