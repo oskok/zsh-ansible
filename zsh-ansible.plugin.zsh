98d21c60661acc92370c948b918d6e475cc5d20e
Skok Oleg <o.skok@yandex.ru>
2025-09-02 11:50:41 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -3,7 +3,7 @@
 # =============================================================================
 
 _zsh_package_pip() {
-  $(which python3) -m pip install --upgrade pip --user >/dev/null
+  $(which python3) -m pip install --upgrade pip --user
   rehash
 }
 
@@ -22,14 +22,16 @@ _zsh_package_ansible() {
     )
 
   for package in "${PACKAGES[@]}"; do
-    $(which python3) -m pip install --upgrade ${package} --user >/dev/null
+    $(which python3) -m pip install --upgrade ${package} --user
     rehash
   done
 }
 
 if type python3 >/dev/null; then
+  setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
   _zsh_package_pip
   _zsh_package_ansible
+  disown &>/dev/null
 fi
 
 if type molecule >/dev/null; then
