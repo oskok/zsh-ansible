dcc2774701e92b2a8edf7b6b7e85d412d25fab89
Skok Oleg <o.skok@yandex.ru>
2025-09-02 11:31:58 +0500

    Initial commit

---
+++ zsh-ansible.plugin.zsh
@@ -0,0 +1,25 @@
+  typeset -r PACKAGES=(
+    'ansible'
+    'ansible-lint'
+    'molecule'
+    'molecule-docker'
+    'tox'
+    'tox-ansible'
+    'hvac'
+    'dnspython'
+    'jmespath'
+    'urllib3<2.0'
+  )
+
+if type python3 >/dev/null; then
+    for package in "${PACKAGES[@]}"; do
+      echo "==> Fetching ${package}"
+      $(which python3) -m pip install --upgrade ${package} --user
+      rehash && echo
+    done
+  fi
+}
+
+if type molecule >/dev/null; then
+  export MOLECULE_NO_LOG='false'
+fi
