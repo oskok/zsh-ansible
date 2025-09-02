318ed77bef74b88dd38faaa13d723a098217042e
Skok Oleg <o.skok@yandex.ru>
2025-09-02 12:14:31 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -2,18 +2,9 @@
 #   ZSH ANSIBLE PLUGIN
 # =============================================================================
 
-_zsh_package_pip() {
-  typeset -r PACKAGES=(
-    'pip'
-  )
-
-  for package in "${PACKAGES[@]}"; do
-    $(which python3) -m pip install --upgrade ${package} --user &>/dev/null
-  done
-  rehash
-}
-
 _zsh_package_ansible() {
+
+  # Список пакетов для установки
   typeset -r PACKAGES=(
     'ansible'
     'ansible-lint'
@@ -27,19 +18,24 @@ _zsh_package_ansible() {
     'urllib3<2.0'
   )
 
+  # Устанавливаем или обновляем PIP до актуальной версии
+  $(which python3) -m pip install --upgrade pip --user &>/dev/null
+
+  # Устанавливаем или обновляем ANSIBLE и его дополнения
   for package in "${PACKAGES[@]}"; do
     $(which python3) -m pip install --upgrade ${package} --user &>/dev/null
   done
   rehash
 }
 
+# Вызов установки в фоне
 if type python3 >/dev/null; then
   setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
-  _zsh_package_pip &
   _zsh_package_ansible &
   disown &>/dev/null
 fi
 
+# Переменные
 if type molecule >/dev/null; then
   export MOLECULE_NO_LOG='false'
 fi
