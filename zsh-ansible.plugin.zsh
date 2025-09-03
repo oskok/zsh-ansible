8a02cf5c2f2913f52e2322e186bbb2211c75d5d1
Skok Oleg <o.skok@yandex.ru>
2025-09-03 09:11:27 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -19,20 +19,26 @@ _zsh_package_ansible() {
   )
 
   # Устанавливаем или обновляем PIP до актуальной версии
-  $(which python3) -m pip install --upgrade pip --user > /dev/null 2>&1 &
+  if ! type ansible; then
+    $(which python3) -m pip install --upgrade pip --user
+  else
+    $(which python3) -m pip install --upgrade pip --user > /dev/null 2>&1 &
+  fi
 
   # Устанавливаем или обновляем ANSIBLE и его дополнения
   for package in "${PACKAGES[@]}"; do
-    $(which python3) -m pip install --upgrade ${package} --user > /dev/null 2>&1 &
+    if ! type ansible; then
+      $(which python3) -m pip install --upgrade ${package} --user
+    else
+      $(which python3) -m pip install --upgrade ${package} --user > /dev/null 2>&1 &
+    fi
   done
   rehash
 }
 
 # Вызов установки в фоне
 if type python3 >/dev/null; then
-  setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
   _zsh_package_ansible
-  disown &>/dev/null
 fi
 
 # Переменные
